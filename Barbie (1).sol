be415f7fe2bd52ebbe1d7b9341b6ac5b19c677fe
Riya Verma <117571193+iriyaverma@users.noreply.github.com>
2024-06-30 16:00:08 +0530

    Add files via upload

---
+++ Barbie (1).sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract BarbieDollShop {
+    uint public dollInventory;
+    uint private dollPriceBuy; 
+    uint private dollPriceSell; 
+    uint public marketFund;
+
+    constructor() {
+        dollInventory = 10; // Initial inventory of 10 dolls
+        dollPriceBuy = 100;
+        dollPriceSell = 200;
+        marketFund = 1000;
+    }
+
+    function buyDolls(uint _amount) public {
+        require(_amount > 0, "Invalid amount");
+        uint totalCost = _amount * dollPriceBuy;
+        if (marketFund < totalCost) {
+            revert("Insufficient funds in market");
+        }
+        marketFund -= totalCost;
+        dollInventory += _amount;
+        assert(dollInventory >= 2);  // Ensure doll inventory is having atleast 2 dolls
+        assert(marketFund >= 200); // Ensure market fund is atleast 200
+    }
+
+    function sellDolls(uint _amount) public {
+        require(_amount > 0, "Invalid amount");
+        if (dollInventory < _amount) {
+            revert("Not enough dolls in inventory");
+        }
+        dollInventory -= _amount;
+        marketFund += _amount * dollPriceSell;
+        assert(dollInventory >= 2); 
+        assert(marketFund >= 200); 
+    }
+}
